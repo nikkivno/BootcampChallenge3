a61272cf3d78502acade1a47c276a4f6807d89d4
nikkivno <nikkivigneault@gmail.com>
2023-05-13 19:25:57 -0300

    Added in if statements for each prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,43 @@
-// Assignment Code
+
+var characterLength = 8
+var choiceArr = [];
+
 var generateBtn = document.querySelector("#generate");
-var characters= [];
-    for (var i = 8; i <= 128; i++) {
-       characters.push(i);
-    }
+
 var numbers = ["0" , "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"];
 var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
 
-console.log(characters)
-console.log(upperCaseLetters)
-console.log(lowerCaseLetters)
-console.log(numbers)
-console.log(specialCharacters)
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  console.log("You clicked the 'Generate Password' Button!");
-  return "Your Password will be displayed here!"
+    characterLength = (prompt ("How many characters do you want in your password? (must be between 8-128)"));
+    
+    if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert ("Password length must be between 8-128 characters long and expressed as a #. Please try again");
+    return false;
+    }
+    ask();
+
+    if (confirm ("Would you like numbers in your password?")) {    
+    choiceArr = choiceArr.concat(numbers);
+    }
+    if (confirm ("Would you like uppercsase letter in your password?")) { 
+    choiceArr = choiceArr.concat(upperCaseLetters);
+    }
+    if (confirm ("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseLetters);
+    }
+    if (confirm ("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharacters);
+    }
+    return true;
   }
 
   
 
-// - open website 
-
-// - click 'Generate Password' button 
-
-// - Prompt user to tell code how many characters they want in their password
-
-// - User writes in a number between 8-128 
-
-// - Prompt user to select what kinds of special characters they want in their code (Uppercase, lowercase, number, symbol)
-    //Do all of these options have to be in different boxes?
-
-// - Make sure info correlates to parameters set
-
-// - Generate password
-
-// - Password is generated in the display box
-
-
-// prompts 
-
-writePassword = prompt ("How many characters do you want in your password? (must be between 8-128)")
-
-writePassword = prompt ("Do you want numbers in your password?")
-
-writePassword = prompt ("Do you want special characters in your program? (ie. !, &, %, $)")
 
 
 // Write password to the #password input
@@ -58,6 +48,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
